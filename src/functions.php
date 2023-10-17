ae4d2ff85810fe5f6eb2278badd0c59750a859fb
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-17 13:39:53 +0200

    add compile

--- src/functions.php
+++ src/functions.php
@@ -3,6 +3,7 @@
 
 require_once "Routes/PublicRoute.php";
 require_once "Routes/SCSS.php";
+require_once "Compiler.php";
 
 require_once "Commands/InstallMainSQLCommandline.php";
 require_once "Commands/InstallMenuSQLCommandline.php";
