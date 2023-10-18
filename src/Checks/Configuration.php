80d18456535236500ee3e4fff90844a0c63f66d3
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-18 21:22:45 +0200

    fixes

--- src/Checks/Configuration.php
+++ src/Checks/Configuration.php
@@ -24,29 +24,5 @@ class Configuration  extends PostCheck {
             }
         }
 
-        $tables = [
-            'ds'=>[
-                'columns'=>[
-                    'table_name'=>'varchar(128)'
-                ]
-            ],
-            'ds_column'=>[],
-            'ds_column_list_label'=>[],
-            'ds_column_form_label'=>[],
-            'view_ds_column'=>['filename'=>'text', 'js'=>'longtext', 'name'=>'varchar(100)', 'table_name'=>'varchar(128)'],
-            'view_ds_combobox'=>['filename'=>'text', 'js'=>'longtext', 'name'=>'varchar(100)', 'table_name'=>'varchar(128)'],
-            'view_ds_controller'=>['filename'=>'text', 'js'=>'longtext', 'jsx'=>'longtext', 'table_name'=>'varchar(128)'],
-            'view_ds_dsview'=>['filename'=>'text', 'js'=>'longtext', 'table_name'=>'varchar(128)'],
-            'view_ds_form'=>['filename'=>'text', 'js'=>'longtext', 'jsx'=>'longtext', 'table_name'=>'varchar(128)'],
-            'view_ds_list'=>['filename'=>'text', 'js'=>'longtext', 'jsx'=>'longtext', 'table_name'=>'varchar(128)'],
-            'view_ds_listcolumn'=>['js'=>'longtext', 'requiresJS'=>'longtext', 'table_name'=>'varchar(128)'],
-            'view_ds_model'=>['filename'=>'text', 'js'=>'longtext', 'name'=>'text', 'table_name'=>'varchar(128)'],
-            'view_ds_store'=>['filename'=>'text', 'js'=>'longtext', 'name'=>'text', 'table_name'=>'varchar(128)']
-        ];
-        self::tableCheck('ds',$tables,
-            "please run the following command: `./tm install-sql-ds --client ".$clientdb->dbname."`",
-            "please run the following command: `./tm install-sql-ds --client ".$clientdb->dbname."`"
-
-        );
     }
 }
