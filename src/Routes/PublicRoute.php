92b733410915e5ce2e25aabef1348bfeffec602a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-06-03 14:07:27 +0200

    fixes

--- src/Routes/PublicRoute.php
+++ src/Routes/PublicRoute.php
@@ -15,6 +15,14 @@ class PublicRoute implements IRoute{
             }
         },['get'],false);
 
+        BasicRoute::add('/bootstrap/css/(?P<file>[\/.\w\d\-\_\.]+)'.'',function($matches){
+            if (file_exists( dirname(__DIR__,2).'/lib/'.$matches['file']) ){
+                App::etagFile( dirname(__DIR__,2).'/lib/'.$matches['file'] , true);
+                BasicRoute::$finished = true;
+                http_response_code(200);
+            }
+        },['get'],false);
+
         BasicRoute::add('/tualocms/page/bootstrap/(?P<file>[\/.\w\d\-\_\.]+)'.'',function($matches){
             if (file_exists( dirname(__DIR__,2).'/lib/'.$matches['file']) ){
                 App::etagFile( dirname(__DIR__,2).'/lib/'.$matches['file'] , true);
