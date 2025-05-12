3156e406b80bbc0dc527ae54b914a89b08788f24
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-05-12 09:24:09 +0200

    add auto compiler

--- src/sql/install/addcommand.sql
+++ src/sql/install/addcommand.sql
@@ -1,2 +1,3 @@
 delimiter ; 
 insert ignore into ds_addcommands_xtypes (id,name) values ('bootstrap_compiler_command','Bootstrap-Kompiler');
+insert ignore into ds_addcommands_xtypes (id,name) values ('bootstrap_autocompiler_command','Bootstrap-Auto-Kompiler');
