e0aa1c8f9358e056aa815c53f329f62cef0fb52c
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-07-21 13:14:37 +0200

    test

--- src/Checks/Configuration.php
+++ src/Checks/Configuration.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\Bootstrap\Checks;
 
 use Tualo\Office\Basic\Middleware\Session;
@@ -6,23 +7,24 @@ use Tualo\Office\Basic\PostCheck;
 use Tualo\Office\Basic\TualoApplication as App;
 
 
-class Configuration  extends PostCheck {
-    
-    public static function test(array $config){
+class Configuration  extends PostCheck
+{
+
+    public static function test(array $config)
+    {
         $clientdb = App::get('clientDB');
         if (is_null($clientdb)) return;
 
-        if(($cmd = App::configuration('scss','cmd',false))==false){
-            PostCheck::formatPrintLn(['red'],"\tscss cmd not found");
-            PostCheck::formatPrintLn(['blue'],"\tcall `./tm configuration --section scss --key cmd --value $(which sass)`");
-        }else{
-            exec($cmd.' --version',$output,$return_var);
-            if ($return_var!=0){
-                PostCheck::formatPrintLn(['red'],"\tscss cmd *$cmd* is not callable ($return_var), try `npm install -g sass`");
-            }else{
-                PostCheck::formatPrintLn(['green'],"\tscss version: ".implode(' ',$output));
+        if (($cmd = App::configuration('scss', 'cmd', false)) == false) {
+            PostCheck::formatPrintLn(['red'], "\tscss cmd not found");
+            PostCheck::formatPrintLn(['blue'], "\tcall `./tm configuration --section scss --key cmd --value $(which sass)`");
+        } else {
+            exec($cmd . ' --version', $output, $return_var);
+            if ($return_var != 0) {
+                PostCheck::formatPrintLn(['red'], "\tscss cmd *$cmd* is not callable ($return_var), try `npm install -g sass`");
+            } else {
+                PostCheck::formatPrintLn(['green'], "\tscss version: " . implode(' ', $output));
             }
         }
-
     }
 }
